79bc22a4fb22c49db768fc11a4e30737426b0a0c
Ruslan <axelezgames@gmail.com>
2023-11-24 14:13:36 +0500

    create clear project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="w-full max-w-7xl bg-yellow-50">
+      <div className="flex flex-row justify-between">
+        <section className=" bg-yellow-100 w-10 h-10"></section>
+        <section></section>
+        <section></section>
+      </div>
+    </main>
+  );
+}
